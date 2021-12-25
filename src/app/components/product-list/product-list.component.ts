d088e164318b28959299f5d1232665d49cac20b6
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-12-26 01:15:53 +0530

    css files

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] | undefined;
+  //inject product service
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.listProducts();
   }
 
+  listProducts() {
+    //method is invoked once you subscribe
+    this.productService.getProductList().subscribe(
+      //assign results to the product array
+      (data) => {
+        this.products = data;
+      }
+    );
+  }
 }
